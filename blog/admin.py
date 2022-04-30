59aefad9ab305d377e4c12afbf590fdfb30fc3aa
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-04-30 18:23:47 +0000

    add post and comment display and search options

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
-
 class PostAdmin (SummernoteModelAdmin):
 
-    summernote_fields= ('body')
+    list_display = ('title', 'author', 'status','created_on')
+    search_fields = ['title', 'body',]
+    prepopulated_fields = {'slug':('title' ,)} 
+    list_filter = ('category', 'created_on')
+    summernote_fields = ('body')
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin (admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
