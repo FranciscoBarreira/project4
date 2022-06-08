9a40175843b6bfdab9b912da6f2c1166ac395b96
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-06-08 17:30:52 +0000

    add test for home page

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,15 +1,19 @@
-from django.test import TestCase
+import unittest
+from django.test import Client, TestCase
 from django.contrib.auth.models import User
 from .models import Post, Comment
 
-class UserTest(TestCase):
-    def user_test(self):
-        """ creates a user """
-        user_test = User.objects.create_user(
-            username='user1', password='userpass'
-            )
-        self.post = Post.objects.create(title='test', author=user_test)
-        self.comment = Comment.objects.create(
-            body='Test Comment', post=self.post
-            )
-        self.client.login(username="user1", password="userpass")
+
+
+    
+
+class TestTemplates(TestCase):
+    """ gets the blog pages to make sure there is a positive response"""
+    def test_get_home(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')            
+
+if __name__ == "__main__":
+    unittest.main()
