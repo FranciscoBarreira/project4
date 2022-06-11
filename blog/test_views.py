b92ec98b59428ea2e32a0956ed75a11799a74465
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-06-11 15:53:02 +0000

    add testing

--- blog/test_views.py
+++ blog/test_views.py
@@ -5,7 +5,16 @@ from .models import Post, Comment
 
 
 
-    
+class TestUser(unittest.TestCase):
+    def test_user(self):
+        """ creates a user, test post and comment """
+        test_user = User.objects.create_user(
+            username='user20', password='userpass'
+            )
+        self.post = Post.objects.create(title='test20', author=test_user)
+        self.comment = Comment.objects.create(
+            body='Test Comment', post=self.post
+            )  
 
 class TestTemplates(TestCase):
     """ gets the blog pages to make sure there is a positive response"""
@@ -14,6 +23,20 @@ class TestTemplates(TestCase):
         response = c.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')            
+
+    def test_get_post_detail(self):
+        post_list = Post.objects.all()
+        for post in post_list:
+            c = Client()
+            response = c.get(f'/post_detail/{self.post.id}')
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_get_search(self):
+        c = Client()
+        response = c.get('/search')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'search.html')        
 
 if __name__ == "__main__":
     unittest.main()
